3a5af9a598358afa1300edd7bf0c5659915e752c
Azizahaul <azizahauliarhamid@gmail.com>
2025-06-10 15:10:57 +0700

    int main try dan catch

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,18 @@
 #include <array>
 //untuk obyek array yang akan digunakan
 using namespace std;
+
+int main(){
+    cout << "Awal Program" << endl; //penanda 1:..
+    try{
+        array<int, 3> data = {10,20,30};
+        //pesan array integer 3 elemen
+        cout <<data.at(5) << endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+}
