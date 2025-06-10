dba8f05ee75b88644f34aa19e19f860ddc1b34fb
Azizahaul <azizahauliarhamid@gmail.com>
2025-06-10 15:37:23 +0700

    membuat nama file dengan menggunakan ios in

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string NamaFile;
+    
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios :: out);
+    cout << "> Menulis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true){
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //lopp akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menuliskan sekarang tutup filenya
+    outfile.close();
+    //membuka file dlam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open(NamaFile, ios::in);
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
